b6a158199468546f38a5b88b7c6b9e44f3248af7
janys <jarrick.nys@ugent.be>
2014-11-05 09:32:27 +0100

    update thesis

--- thesis/python code/script.py
+++ thesis/python code/script.py
@@ -3,30 +3,30 @@ import scipy as sp
 import numpy as np
 from scipy import special
 from scipy.integrate import quad
-#hbar and c =1
 
-M = 0.00939272 #mass nucleon in 10^2GeV
+hbarc = 197.327 #(MeV*fm)
+M = 939.272 #mass nucleon (MeV)
 A = 40 #mass number Argon
-Omega = (45*(A**(-1./3.)))-(25*(A**(-2./3.)))*10**(-5) #parametrization for hbar*Omega for HO in 10^2GeV
-v = M*Omega 
-v_mom = 1/(Omega*M) 
+Omega = (45*(A**(-1./3.)))-(25*(A**(-2./3.)))#parametrization for hbar*Omega for HO (MeV)
+v = M*Omega / hbarc
+v_mom = hbarc / (Omega*M) 
 
 #quantum numbers
-n = 2
-l = 2
+n = 0
+l = 0
 
 #returns unnormalized radial wavefunction calculated with constant a 
 def Rad_wavefunc(r, a, n, l):
 	t = a*(r**2)
 	return (r**l)*sp.special.eval_genlaguerre(n, l+0.5, t)*np.exp(-(a/2.)*r**2)
 
-f = open("data.dat", "w")	
-i = 0	
-while (i < 3000):
-	txt = str(Rad_wavefunc(0.001*i, v_mom, n, l)) + '\t' + str(0.001*i)
+f = open("data.txt", "w")	
+i = 0
+while (i < 5*10**(9)):
+	txt = str(Rad_wavefunc(i*10**(-8), v_mom, n, l)) + '\t' + str(i*10**(-8))
 	f.write( txt  ) 
 	f.write('\n')
-	i = i+1
+	i = i+ (10**(7))
 
 def norm(a, n, l):
 	return np.sqrt((2*m.factorial(n)*a**(l+(3./2.)))/m.gamma(n+l+(3./2.)))
