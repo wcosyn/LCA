4e6e9d993614731b8fca5f43c65444a997386cd2
janys <jarrick.nys@gugent.be>
2015-03-02 17:22:43 +0100

    update

--- thesis/python_code/helium.py
+++ thesis/python_code/helium.py
@@ -41,31 +41,42 @@ def Rad_wavefunc(k, a, n, l):
 def norm(a, n, l):
 	return np.sqrt((2*math.factorial(n)*a**(l+(3./2.)))/math.gamma(n+l+(3./2.)))
      
+def integrand_norm(k,n,l):
+     return (Rad_wavefunc(k, v_mom(M_average,0), n, l)*norm(v_mom(M_average,0), n, l)*k)**2
+
+     
+def normalis(n,l):
+     nor, err = sp.integrate.quad(integrand_norm,0,20, args= (n,l))
+     return nor
+     
+
+print normalis(0,0)    
 #returns spherical bessel function of order n in point z     
 def sph_Bessel(n,z):
      x = sp.special.sph_jn(n,z)
      return x[0][n]
      
-#integrand for integrals thesis M. Vanhalst p.75 eq.16
-def integr(r,n,l, M_w, i,k):
-     return r**2*Rad_wavefunc(r*np.sqrt(2), v(M_w,i), n, l)*norm(v(M_w,i), n, l)*sph_Bessel(n, k*r)
-     
-#integral thesis M. Vanhalst p.75 eq.16
-def wave_part(n, l, M_w, i,k): 
-     result, err = sp.integrate.quad(integr,0, 10, args=(n,l, M_w, i,k))
-     return result
-     
+f = open("bessel.txt", "w")
+i = 0
+while (i < 2001):
+     d = sph_Bessel(0,0.01*i)
+     txt = str(0.01*i) + '\t' + str((d))  
+     f.write(txt)
+     f.write('\n')
+     i = i+1
+f.close()
+  
+                         for S in xrange(0,2):
+                              for M_S in xrange(-S,S+1):
 #rechtstreekse input van k-space golffuncties Rad_wavefunc(P, v_mom, N, L)
 def twobody_1(k, M_w, i, mt_1, mt_2, n1 , n2):
      result = 0
      for ms_1 in xrange(0, 2):
           for ms_2 in xrange(0, 2):
                for S in xrange(0, 2):
-                    for M_S in xrange(-S, S+1):
-                         for T in xrange(0, 2):
-                              for M_T in xrange(-T, T+1):
-                                   if(ms_1 != ms_2 or mt_1 != mt_2):
-                                        result = result + ((1-(-1)*(S+T))**2)*(clebsch_gordan(0.5,0.5,S,ms_1-0.5,ms_2-0.5,M_S)**2)*(clebsch_gordan(0.5,0.5,T,mt_1,mt_2,M_T)**2)*Rad_wavefunc(k/np.sqrt(2), v_mom(M_w,i), 0, 0)*Rad_wavefunc(k/np.sqrt(2), v_mom(M_w,i), 0, 0)*norm(v_mom(M_w,i), 0, 0)*norm(v_mom(M_w,i), 0, 0)
+                    for T in xrange(0, 2):
+                         if(ms_1 != ms_2 or mt_1 != mt_2):
+                              result = result + ((1-(-1)**(S+T))**2)*(clebsch_gordan(0.5,0.5,S,ms_1-0.5,ms_2-0.5,ms_1 + ms_2 - 1)**2)*(clebsch_gordan(0.5,0.5,T,mt_1,mt_2,mt_1 + mt_2)**2)*Rad_wavefunc(k/np.sqrt(2), v_mom(M_w,i), 0, 0)*Rad_wavefunc(k/np.sqrt(2), v_mom(M_w,i), 0, 0)*norm(v_mom(M_w,i), 0, 0)*norm(v_mom(M_w,i), 0, 0)
      return (1/16.)*result
      
 #berekening eq 16 p75 thesis M. Vanhalst
@@ -78,8 +89,8 @@ def twobody_2(k, M_w, i, mt_1, mt_2, n1 , n2):
                          for T in xrange(0, 2):
                               for M_T in xrange(-T, T+1):
                                    if(ms_1 != ms_2 or mt_1 != mt_2):
-                                        result = result + ((1-(-1)*(S+T))**2)*(clebsch_gordan(0.5,0.5,S,ms_1-0.5,ms_2-0.5,M_S)**2)*(clebsch_gordan(0.5,0.5,T,mt_1,mt_2,M_T)**2)*wave_part(0,0, M_w, i,k)*wave_part(0, 0, M_w, i,k)
-     return (1./np.pi)*result
+                                        result = result + ((1-(-1)**(S+T))**2)*(clebsch_gordan(0.5,0.5,S,ms_1-0.5,ms_2-0.5,M_S)**2)*(clebsch_gordan(0.5,0.5,T,mt_1,mt_2,M_T)**2)*wave_part(0,0, M_w, i,k)*wave_part(0, 0, M_w, i,k)
+     return (1./math.pi)*result
      
 def opvulling_1(k,Q_1, Q_2, M_w, i, mt_1, mt_2):
      return twobody_1(k, M_w, i, mt_1, mt_2,0, 0)
@@ -94,37 +105,39 @@ def opvulling_2(k,Q_1, Q_2, M_w, i, mt_1, mt_2):
      
 #twobody summation for pp, nn and pn
 def distribution_2(k, i):
-     return (1.0/(A[i]*(A[i]-1)))*(opvulling_2(k, Z[i], Z[i],M_average, i, 0.5, 0.5) + opvulling_2(k,N[i], N[i], M_average, i, -0.5, -0.5) + opvulling_2(k, Z[i], N[i],M_average, i, 0.5, -0.5) + opvulling_2(k, N[i], Z[i],M_average, i, -0.5, 0.5))
+     return (1.0/(A[i]*(A[i]-1)))*(opvulling_2(k, Z[i], Z[i],M_average, i, -0.5, -0.5) + opvulling_2(k,N[i], N[i], M_average, i, 0.5, 0.5) + opvulling_2(k, Z[i], N[i],M_average, i, -0.5, 0.5) + opvulling_2(k, N[i], Z[i],M_average, i, 0.5, -0.5))
      
 def integrand(k,i):
-     return distribution_2(k,i)*k**2
-#helpfunction: round values     
-def format(value):
+     return distribution_1(k,i)*(k**2)
+#helpfunction: round values
+def format_(value):
     return "%.8f" % value
 
 nuclides = [0]
 distr_array_1 = []
 distr_array_2 = []
+step = 0.1
+upperlimit = 25
 for j in nuclides:
      f = open("{:s}_mf_twobody.txt".format(nuc[j]), "w")
      f.write("# mass number (A) = " + str(A[j]) +  '\n')
      f.write("# number of protons (Z) = " + str(Z[j]) +'\n')
-     f.write("# k (1/fm)" + '\t' + "mf (fm^3)" + '\n')
+     f.write("# k (1/fm)" + '\t' + "mf_zelf (fm^3)" + '\t' + "mf_maarten (fm^3)" + '\n')
      print str(A[j])
      i = 0
-     while (i < 50):
-          print i
-          c = distribution_1(0.1*i, j)
-          d = distribution_2(0.1*i, j)
-          distr_array_1.append(c*(0.1*i)**2)
-          distr_array_2.append(d*(0.1*i)**2)
-          txt = str(0.1*i) + '\t' + str(format(c)) + '\t' + str(format(d))  + '\t' + str(format(c/d))
+     while (i < upperlimit):
+          c = distribution_1(step*i, j)
+          d = distribution_2(step*i, j)
+          distr_array_1.append(c*(step*i)**2)
+          distr_array_2.append(d*(step*i)**2)
+          txt = str(step*i) + '\t' + str(format_(c)) + '\t' + str(format_(d))  + '\t' + str(format_(c/d))
           f.write(txt)
           f.write('\n')
           i = i+1
-     nor_1 = sp.integrate.trapz(distr_array_1,x=None,dx = 0.1)
-     nor_2 = sp.integrate.trapz(distr_array_2,x=None,dx = 0.1)
-     f.write("#normalisation = " + str(nor_1) + '\t' + str(nor_2) + '\n') 
+     nor_1 = sp.integrate.trapz(distr_array_1,x=None,dx = step)
+     nor_2 = sp.integrate.trapz(distr_array_2,x=None,dx = step)
+     nor = sp.integrate.quad(integrand, 0, 5, args=(j) )
+     f.write("#normalisation = " + str(nor_1) + '\t' + str(nor_2) + '\t' + str(nor) +'\n') 
      f.close()
 
    
